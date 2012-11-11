b3c20556e6e73a0f4ffe8cacb2a55ae2d3b287ba
dwang9 <dwang9@stc82.complab.uci.edu>
2012-11-11 12:54:57 -0800

    GetHelp

---
+++ MedAppJam/GetHelp.h
@@ -0,0 +1,15 @@
+//
+//  GetHelp.h
+//  MedAppJam
+//
+//  Created by dwang9 on 11/11/12.
+//  Copyright (c) 2012 AppJam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GetHelp : NSObject
+
+
+
+@end
---
+++ MedAppJam/GetHelp.m
@@ -0,0 +1,13 @@
+//
+//  GetHelp.m
+//  MedAppJam
+//
+//  Created by dwang9 on 11/11/12.
+//  Copyright (c) 2012 AppJam. All rights reserved.
+//
+
+#import "GetHelp.h"
+
+@implementation GetHelp
+
+@end
