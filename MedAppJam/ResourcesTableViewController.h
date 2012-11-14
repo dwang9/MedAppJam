56a5994bc3eba32836c0386a50a5587a94d5b5a6
jpuente <jpuente@stc74.complab.uci.edu>
2012-11-13 20:33:10 -0800

    a

---
+++ MedAppJam/ResourcesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResourcesTableViewController.h
+//  MedAppJam
+//
+//  Created by jpuente on 11/13/12.
+//  Copyright (c) 2012 AppJam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ResourcesTableViewController : NSObject
+
+@end
---
+++ MedAppJam/ResourcesTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  ResourcesTableViewController.m
+//  MedAppJam
+//
+//  Created by jpuente on 11/13/12.
+//  Copyright (c) 2012 AppJam. All rights reserved.
+//
+
+#import "ResourcesTableViewController.h"
+
+@implementation ResourcesTableViewController
+
+@end
